81052f1150df4daabc6df414f2d88fe30f287bae
Leonid Nifantyev <leonif1@gmail.com>
2020-12-11 19:35:10 +0200

    first lesson

--- WelcomeToSwift/main.swift
+++ WelcomeToSwift/main.swift
@@ -7,5 +7,49 @@
 
 import Foundation
 
-print("Hello, World!")
+
+let employList: [String] = [
+    "Геннадий Новиков",
+    "Давид С",
+    "Давид С",
+]
+
+print(employList)
+
+let employListSet: Set<String> = [
+    "Геннадий Новиков",
+    "Давид С",
+    "Давид С"
+]
+
+print(employListSet)
+
+
+let isSergeyInList = employList.contains("Давид С")
+
+print(isSergeyInList)
+
+
+
+var r: Double? = .none //server закончил работу с ошибкой
+
+
+
+if let value = r {
+    print(value)
+} else {
+    print("Server ничего не вернул")
+}
+
+let result = r ?? 0
+
+
+switch r {
+case .none: print("Server ничего не вернул")
+case let .some(value): print(value)
+}
+
+
+
+
 
